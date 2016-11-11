24647e67348decb55477a1e65949779ba17621af
Ricardo-Pascual <textex204@gmail.com>
2016-11-11 13:01:44 -0600

    Refactor graph.js

--- graph.js
+++ graph.js
@@ -1,52 +1,52 @@
-function makeSymmetric(matrix) {
-    for (var i = 0; i < matrix.length; ++i) {
-        for (var j = 0; j < i; ++j) {
-            g[i][j] = g[j][i];
-        }
+function makeSymmetric (matrix) {
+  for (var i = 0; i < matrix.length; ++i) {
+    for (var j = 0; j < i; ++j) {
+      g[i][j] = g[j][i]
     }
+  }
 }
 
-function createMatrix(numNodes) {
-    var g = [];
-    for (var i = 0; i < numNodes; ++i) {
-        g[i] = [];
-        for (var j = 0; j < numNodes; ++j) {
-            g[i][j] = Number.POSITIVE_INFINITY;
-        }
-        g[i][i] = 0;
+function createMatrix (numNodes) {
+  var g = []
+  for (var i = 0; i < numNodes; ++i) {
+    g[i] = []
+    for (var j = 0; j < numNodes; ++j) {
+      g[i][j] = Number.POSITIVE_INFINITY
     }
-    return g;
+    g[i][i] = 0
+  }
+  return g
 }
 
-function createGraphExample() {
-    g = createMatrix(10);
-    g[0][1] = 7;
-    g[0][2] = 7;
-    g[2][3] = 2;
-    g[4][5] = 3;
-    g[5][6] = 7;
-    g[4][7] = 2;
-    g[3][4] = 5;
-    g[2][8] = 6;
-    g[1][9] = 5;
-    makeSymmetric(g);
-    return g;
+function createGraphExample () {
+  g = createMatrix(10)
+  g[0][1] = 7
+  g[0][2] = 7
+  g[2][3] = 2
+  g[4][5] = 3
+  g[5][6] = 7
+  g[4][7] = 2
+  g[3][4] = 5
+  g[2][8] = 6
+  g[1][9] = 5
+  makeSymmetric(g)
+  return g
 }
 
-function createRandomGraph(numNodes, edgeProbability, maxWeight) {
-    var g = [];
-    for (var i = 0; i < numNodes; ++i) {
-        g[i] = [];
+function createRandomGraph (numNodes, edgeProbability, maxWeight) {
+  var g = []
+  for (var i = 0; i < numNodes; ++i) {
+    g[i] = []
+  }
+  for (var i = 0; i < numNodes; ++i) {
+    for (var j = i + 1; j < numNodes; ++j) {
+      var randomNumber = Math.random()
+      var length = Number.POSITIVE_INFINITY
+      if (randomNumber > edgeProbability) {
+        length = Math.floor(Math.random() * maxWeight)
+      }
+      g[i][j] = g[j][i] = length
     }
-    for (var i = 0; i < numNodes; ++i) {
-        for (var j = i+1; j < numNodes; ++j) {
-            var randomNumber = Math.random();
-            var length = Number.POSITIVE_INFINITY;
-            if (randomNumber > edgeProbability) {
-                length = Math.floor(Math.random()*maxWeight);
-            }
-            g[i][j] = g[j][i] = length;
-        }
-    }
-    return g;
+  }
+  return g
 }
